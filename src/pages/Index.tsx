b26e73ec29f22b9f846a2ce796749d572e98bc04
Mythripaluri <mythrip737@gmail.com>
2025-10-02 18:22:06 +0530

    README.md

--- src/pages/Index.tsx
+++
@@ -1,8 +0,0 @@
-// This file redirects to the main Home page
-import { Navigate } from "react-router-dom";
-
-const Index = () => {
-  return <Navigate to="/" replace />;
-};
-
-export default Index;
